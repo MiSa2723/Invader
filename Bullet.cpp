bb5cf7d8714c5c84a9c2606b66cf93dd20c2da4c
240143 <240143@jc-21.jp>
2025-05-20 12:59:54 +0900

    Bulletクラスを作って、プレイヤーが放つ弾を実装した。だが、弾を放つごとにメモリが増えていくため、次の時間でそれをどうにかする。

---
+++ Bullet.cpp
@@ -0,0 +1,64 @@
+#include "globals.h"
+#include "Bullet.h"
+#include <DxLib.h>
+#include <string>
+
+namespace
+{
+	const int BULLET_IMAGE_WIDTH = 13;		//弾の画像の幅
+	const int BULLET_IMAGE_HEIGHT = 33;		//弾の画像の高さ
+	const float BULLET_INIT_SPEED = 200.0f;	//弾の初期移動速度
+	const std::string BULLET_IMAGE_PATH = "Assets\\画像\\laserBlue01.png";	//弾の画像パス
+}
+
+Bullet::Bullet()
+	:GameObject(),hBulletImage_(-1),x_(0),y_(0),isAlive_(true),
+	imageSize_x(BULLET_IMAGE_WIDTH),imageSize_y(BULLET_IMAGE_HEIGHT)
+{
+	hBulletImage_ = LoadGraph(BULLET_IMAGE_PATH.c_str());	//弾の画像を読み込む
+	speed_ = BULLET_INIT_SPEED;	//移動速度
+	AddGameObject(this);
+}
+
+Bullet::Bullet(float x, float y)
+	:GameObject(), hBulletImage_(-1), x_(x), y_(y), isAlive_(true), 
+	imageSize_x(BULLET_IMAGE_WIDTH), imageSize_y(BULLET_IMAGE_HEIGHT)
+
+{
+	hBulletImage_ = LoadGraph(BULLET_IMAGE_PATH.c_str());	//弾の画像を読み込む
+	speed_ = BULLET_INIT_SPEED;	//移動速度
+	AddGameObject(this);
+}
+
+Bullet::~Bullet()
+{
+	if (hBulletImage_ != -1)
+	{
+		DeleteGraph(hBulletImage_);	//画像の解放
+	}
+	hBulletImage_ = -1;	//画像ハンドルを無効化
+}
+
+void Bullet::Update()
+{
+	float dt = GetDeltaTime();	//デルタタイムの取得
+	y_ = y_ - (speed_ * dt);	//弾の移動
+	if (y_ < 0)
+	{
+		isAlive_ = false;		//画面外に出たら弾を消す
+	}
+}
+
+void Bullet::Draw()
+{
+	if (isAlive_)
+	{
+		DrawExtendGraph(x_, y_, x_ + imageSize_x, y_ + imageSize_y, hBulletImage_, TRUE);	//弾の描画
+	}
+}
+
+void Bullet::SetPosition(float x, float y)
+{
+	x_ = x;
+	y_ = y;
+}
---
+++ Bullet.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameObject.h"
+
+class Bullet :
+    public GameObject
+{
+private:
+    int hBulletImage_;      //弾の画像のハンドル
+    float x_, y_;           //弾の座標
+    float speed_;           //弾の移動速度
+    bool isAlive_;          //弾が生きているかどうか
+    int imageSize_x, imageSize_y;   //弾の表示サイズ
+
+public:
+    Bullet();
+    Bullet(float x, float y);
+    ~Bullet();
+    void Update() override;
+    void Draw() override;
+    void SetPosition(float x, float y);
+};
+
